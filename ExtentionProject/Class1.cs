45472a7a0abee4acf1f0e40c4dbb5b8cddafd7f6
Noel Adrien <adrien.noel01@gmail.com>
2022-03-16 11:17:07 +0100

    ExtentionComit

---
+++ ExtentionProject/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtentionProject
+{
+  public class ExtentionClassSource
+  {
+    public int Id { get; set; }
+    public int Calcul(int nombre)
+    {
+      return nombre * 100;
+    }
+  }
+}
