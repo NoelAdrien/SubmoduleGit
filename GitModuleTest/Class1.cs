4bbba318cff19cc27055e29b5817617616eb276b
Noel Adrien <adrien.noel01@gmail.com>
2022-03-16 11:32:46 +0100

    add submodule and update main

--- GitModuleTest/Class1.cs
+++ GitModuleTest/Class1.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using SubModuleExtentionSln;
 
 namespace GitModuleTest
 {
   public class Class1
   {
+    public void Test()
+    {
+      var test = new Customer();
+      int dette = test.GetTune();
+    }
   }
 }
